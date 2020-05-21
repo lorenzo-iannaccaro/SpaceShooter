6adedd8a9c55328cea37cd8b20f3ec85d4c55eee
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-05-21 15:52:42 +0200

    add viewport boundaries and padding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] float movementSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    float xMin, xMax, yMin, yMax;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetupMovementBoundaries();
+    }
+
+    private void SetupMovementBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -20,14 +33,15 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
+        // Get input movement
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
-        //Debug.Log(deltaX);
-        var newXPos = transform.position.x + deltaX;
-
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
-        //Debug.Log(deltaY);
-        var newYPos = transform.position.y + deltaY;
 
+        // Calculate new position, within viewport
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        // Set new position
         transform.position = new Vector2(newXPos, newYPos);
     }
 }
