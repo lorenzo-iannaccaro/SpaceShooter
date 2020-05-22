85b3efc854cb9093803c023d9c858036208fadba
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-05-22 13:06:25 +0200

    add enemy spawner script

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+
+    int startWaveIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startWaveIndex];
+        StartCoroutine(SpawnEnemiesInWave(currentWave));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator SpawnEnemiesInWave(WaveConfig waveConfig)
+    {
+        for(int i = 0; i < waveConfig.GetEnemiesPerWave(); i++)
+        {
+            Instantiate(waveConfig.GetEnemyPrefab(),
+                    waveConfig.GetWaypoints()[0].transform.position,
+                    Quaternion.identity);
+
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawn());
+        }
+    }
+}
