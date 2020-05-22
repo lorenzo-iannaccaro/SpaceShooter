ab87cdd186013b3130076b740393cca15b4a5ff6
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-05-22 10:30:46 +0200

    make enemy move through waypoints and destroy it at the end of path

--- Assets/Scripts/EnemyPath.cs
+++ Assets/Scripts/EnemyPath.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class EnemyPath : MonoBehaviour
 {
     [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 5f;
+
+    int waypointIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+
+    }
+
+    private void Move()
+    {
+        if (waypointIndex < waypoints.Count)
+        {
+            float moveSpeedThisframe = moveSpeed * Time.deltaTime;
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            transform.position =
+                Vector2.MoveTowards(transform.position, targetPosition, moveSpeedThisframe);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
