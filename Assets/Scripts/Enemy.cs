121568955c94c89d5a90b2ab0ca195d127c3743e
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-05-22 17:04:41 +0200

    add damage dealer and enemy scripts, update laser to be a trigger, add collider to enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        DamageDealer otherObjectDamageDealer = otherObject.GetComponent<DamageDealer>();
+        health -= otherObjectDamageDealer.GetDamage();
+    }
+}
