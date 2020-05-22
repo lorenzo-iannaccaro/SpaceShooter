fe5512a6df7bdadff9b0a794f90e5212dae740b5
Lorenzo Iannaccaro <lorenzo.iannaccaro@gmail.com>
2020-05-22 10:58:37 +0200

    add waveconfig

---
+++ Assets/Scripts/WaveConfig.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Enemy wave configuration")] 
+
+public class WaveConfig : ScriptableObject
+{
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] int enemiesPerWave = 5;
+    [SerializeField] float enemyMoveSpeed = 5f;
+    [SerializeField] float timeBetweenSpawn = 0.4f;
+    [SerializeField] float spawnTimeRandomicity = 0.2f;
+
+    public GameObject GetEnemyPrefab()
+    {
+        return enemyPrefab;
+    }
+    public GameObject GetPathPrefab()
+    {
+        return pathPrefab;
+    }
+    public int GetEnemiesPerWave()
+    {
+        return enemiesPerWave;
+    }
+    public float GetEnemyMoveSpeed()
+    {
+        return enemyMoveSpeed;
+    }
+    public float GetTimeBetweenSpawn()
+    {
+        return timeBetweenSpawn;
+    }
+    public float GetSpawnTimeRandomicity()
+    {
+        return spawnTimeRandomicity;
+    }
+
+
+}
